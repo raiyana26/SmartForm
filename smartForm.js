92f13dd2699164b48e2c0644051baeb0ceb7b85d
raiyana26 <raiyanasharafat@gmail.com>
2017-06-14 20:09:33 +0600

    Assignment 7&8

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,26 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+	name:"",
+	email:""
+	html: {likes: [], dislikes:[]},
+	css: {likes: [], dislikes:[]},
+	js: {likes: [], dislikes:[]},
+};
+
+$("#startBtn").click(function(event){
+	consile.log("Start Clicked")
+	$("#welcome").hide();
+	$("#q1").show();
+});
+
+$("#name").change(function(event){
+	consile.log($("#name".val());
+		if ("#name").val()){
+		userData.name = $("#name").val();
+	}
+});
+	$("#email").change(function(event){
+		
+	});
+
